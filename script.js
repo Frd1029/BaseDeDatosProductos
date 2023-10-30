f09ccd71b0a959468ff3fdbe0c181b954eb29173
Javier Rincon <javiersneiderrm@ufps.edu.co>
2023-10-30 18:12:21 -0500

    Botones de actualizar y eliminar en el index

--- script.js
+++ script.js
@@ -9,9 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
                 const cell1 = row.insertCell(0);
                 const cell2 = row.insertCell(1);
                 const cell3 = row.insertCell(2);
+                const cell4 = row.insertCell(3);
+
+                const botonEditar = document.createElement("button");
+                botonEditar.textContent = "Actualizar";
+                botonEditar.addEventListener("click", function() {
+                  window.location.href = "actualizar_productos.html";
+                });
+          
+                const botonEliminar = document.createElement("button");
+                botonEliminar.textContent = "Eliminar";
+                botonEliminar.addEventListener("click", function() {
+                  alert("Eliminar " + producto.nombre);
+                });
+          
+                cell4.appendChild(botonEditar);
+                cell4.appendChild(botonEliminar);
                 cell1.textContent = producto.nombre ;
                 cell2.textContent = producto.precio;
                 cell3.textContent = producto.descripcion ;
+
             });
         })
         .catch(error => {
